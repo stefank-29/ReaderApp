4e9a4abb5b529a50ea822839f3e5432552a2a7a1
stefank-29 <stefankaraferovic@gmail.com>
2021-07-08 18:39:15 +0200

    search form

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,53 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import { useState } from 'react';
+import FormStyles from '../styles/FormStyles';
+import styled from 'styled-components';
+import ButtonStyles from '../styles/ButtonStyles';
+import { FaSearch } from 'react-icons/fa';
+
+const HomeStyles = styled.main`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+`;
 
 export default function HomePage() {
-  return (
-   <div></div>
-  )
+    const [searchQuery, setSearchQuery] = useState();
+    const [disabledBtn, setDisabledBtn] = useState(true);
+
+    function handleChange(e) {
+        let query = e.target.value;
+        if (query !== '') {
+            setDisabledBtn(false);
+        } else {
+            setDisabledBtn(true);
+        }
+        setSearchQuery(query);
+    }
+
+    function handleSubmit() {}
+
+    return (
+        <>
+            <Head>
+                <title>My library</title>
+            </Head>
+            <HomeStyles>
+                <FormStyles onSubmit={handleSubmit}>
+                    <input
+                        className="search-input"
+                        onChange={handleChange}
+                        placeholder="Search books, articles or magazines..."
+                        type="text"
+                        value={searchQuery}
+                    />
+                    <ButtonStyles className="submit-btn" disabled={disabledBtn}>
+                        <FaSearch className="icon" />
+                        <span>Search</span>
+                    </ButtonStyles>
+                </FormStyles>
+            </HomeStyles>
+        </>
+    );
 }
---
+++ styles/FormStyles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+const FormStyles = styled.form`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    margin-top: 4rem;
+    .search-input {
+        flex: 1;
+        margin-right: 2rem;
+        border: 2px solid var(--lightPurple);
+        border-radius: 4px;
+        padding: 1.5rem;
+        font-size: 1.6rem;
+        outline: none;
+        background-color: #fefefeaa;
+        ::placeholder {
+            font-weight: 700;
+        }
+        :hover,
+        :focus {
+            box-shadow: 0 0 4px 1px #668aff77;
+        }
+    }
+`;
+
+export default FormStyles;
